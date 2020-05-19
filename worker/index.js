84589968dc27ddb15a1a15be6f962dfbea494fad
Thanadon Pakawatthippoyom <yoyothunder@gmail.com>
2020-05-19 18:19:10 +0700

    initial code and update gitignore

---
+++ worker/index.js
@@ -0,0 +1,23 @@
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000
+});
+
+const sub = redisClient.duplicate();
+console.log(keys)
+function fib(index) {
+    console.log(index)
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+    console.log("ENTERED");
+    
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
